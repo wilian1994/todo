f28b3a1324113321246dbe45336aac0df6699d21
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-09-14 12:08:13 -0300

    Created method remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas'
+
+  //always call this method
+  constructor() {
+    this.todos.push(new Todo(1, 'Jogar futebol', true))
+    this.todos.push(new Todo(3, 'Jogar Poker', false))
+    
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo)
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  maskAsDone(){
+
+  }
+
+  markAsUndone(){
+
+  }
 }
